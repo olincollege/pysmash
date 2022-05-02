7ad1ed6a1152adc97dcfe82b1d92b7d4bdd23fff
Amit Kumar <amitkh@gmail.com>
2022-05-02 09:09:41 -0400

    lives, 2 players, mirrored images, jump vel

--- controller.py
+++ controller.py
@@ -64,6 +64,45 @@ class KeyboardController(Controller):
                 elif event.key in [pygame.K_UP, pygame.K_SPACE]:
                     self.player.jump()
                 else:
+                    pygame.event.post(event)
+                    self.player.normal()
+
+        self.player.move()
+
+class KeyboardController2(Controller):
+    """
+    Controller that allows keyboard control of character
+    """
+    def move(self):
+        """
+        Takes keyboard input and moves Player object accordingly
+        """
+        self.player.gravity()
+
+        # Support for keeping a key held down
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.player.left()
+        if keys[pygame.K_d]:
+            self.player.right()
+        # if keys[pygame.K_w]:
+        #     self.player.jump()
+
+        if keys[pygame.K_a]:
+            self.player.attack()
+        elif keys[pygame.K_d]:
+            self.player.defense()
+        elif keys[pygame.K_w]:
+            self.player.power()
+
+        # Keys that must be repressed
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    self.player.crouch()
+                elif event.key == pygame.K_w:
+                    self.player.jump()
+                else:
                     self.player.normal()
 
         self.player.move()
--- game.py
+++ game.py
@@ -5,7 +5,7 @@ Main PySmash Game Class
 import pygame
 
 from view import WindowView
-from controller import KeyboardController
+from controller import KeyboardController, KeyboardController2
 from characters.mario import Mario
 from stages.final_destination import FinalDestination
 
@@ -13,7 +13,7 @@ class Game:
     """
     Main PySmash Game Class
     """
-    def __init__(self, player1):
+    def __init__(self, player1, player2):
         """
         Create Game instance, define clock, player, controllers, viewers, and
         sprite groups
@@ -22,12 +22,16 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.player1 = player1
+        self.player2 = player2
         self.p1controller = KeyboardController(self.player1)
-        self.all_sprites = pygame.sprite.Group()
-        self.all_sprites.add(self.player1)
+        self.p2controller = KeyboardController2(self.player2)
+        self.all_sprites = pygame.sprite.Group(self.player1, self.player2)
+        # self.all_sprites.add(self.player1)
+        # self.all_sprites.add(self.player2)
 
         self.stage = FinalDestination()
         self.player1.platforms = self.stage.platforms
+        self.player2.platforms = self.stage.platforms
 
         self.viewer = WindowView(self, 1240, 720)
 
@@ -37,10 +41,10 @@ class Game:
         """
         while True:
             self.p1controller.move()
-            print(self.player1.pos, self.player1.vel)
+            self.p2controller.move()
             self.viewer.draw()
             self.clock.tick(60)
 
 if __name__ == "__main__":
-    game = Game(Mario())
+    game = Game(Mario('left'), Mario('right'))
     game.mainloop()
